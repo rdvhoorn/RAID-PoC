1db42664ee7b6b6ee53e6fe1c152113f18acd0d6
r.d.vanhoorn <r.d.vanhoorn@amsterdamumc.nl>
2025-07-16 15:16:28 +0200

    Updated logging config to have service as context variable instead of bound to the logger

--- utils/logging_config.py
+++ utils/logging_config.py
@@ -7,15 +7,21 @@ from utils.config import config
 _configured = False
 
 def configure_logging(dev_mode) -> None:
-    # Set timestamp format
-    timestamper = structlog.processors.TimeStamper(fmt="iso")
-
     # Add processors to standard logging
     shared_processors = [
-        timestamper, # timestamps in abovedefined format
-        structlog.processors.add_log_level,         # i.e. info, error
-        structlog.processors.StackInfoRenderer(),   # adds stack traces if log.exception is called
-        structlog.processors.format_exc_info,       # formats exception tracebacks
+        # Adds a "level" field to each log (e.g. "info", "error") so it's queryable in log systems
+        structlog.processors.add_log_level,
+
+        # If you use log.exception(), this adds the full stack trace into the log entry
+        structlog.processors.StackInfoRenderer(),
+
+        # Formats exception tracebacks (from exc_info=True or log.exception) nicely
+        structlog.processors.format_exc_info,
+
+        # Adds an ISO-8601 timestamp under the "timestamp" key (default output format for most log systems)
+        structlog.processors.TimeStamper(fmt="iso"),
+        
+        structlog.contextvars.merge_contextvars
     ]
 
     # If in devmode, add console colors, otherwise, just log jsons without colors (for prod env)
@@ -39,19 +45,18 @@ def configure_logging(dev_mode) -> None:
         level=logging.INFO,
     )
 
+
 def get_and_configure_logger(service: str = None, dev_mode=config["DEVELOPER_MODE"]) -> Logger:
-    """ Gets a configured logger for your service """
     global _configured
     if not _configured:
-        # Configure logger
         configure_logging(dev_mode)
         _configured = True
-        
-        # Emit "logger_initialized" after setup
-        log = structlog.get_logger()
-        init_log = log.bind(service=service) if service else log
-        init_log.info("logger_initialized")
 
-    # Get logger to return
-    log = structlog.get_logger()
-    return log.bind(service=service) if service else log
+        # Bind global context
+        if service:
+            structlog.contextvars.bind_contextvars(service=service)
+
+        structlog.get_logger().info("logger_initialized")
+
+    return structlog.get_logger()
+
