c16732fde8bf74a06061443528a6583e7bd64a4f
r.d.vanhoorn <r.d.vanhoorn@amsterdamumc.nl>
2025-07-17 16:40:25 +0200

    fixed run_task for new setup

--- run_task.py
+++ run_task.py
@@ -7,7 +7,7 @@ from utils.config import config
 from pathlib import Path
 
 wsi_id = "abc123"
-tool_name = "cellvit"
+tool_name = "cellvit++"
 output_dir = Path(config["INFERENCE_OUTPUT_DIR"])
 
 # Step 1: Submit job via API
@@ -17,33 +17,29 @@ if response:
 else:
     exit(1)
 
-# # Step 2: First status check (should still be running)
-# time.sleep(2)
-# status = check_job_status(wsi_id, tool_name)
-# if status:
-#     print("[2] First status check:", status)
+# Step 2: First status check (should still be running)
+time.sleep(2)
+status = check_job_status(wsi_id, tool_name)
+if status:
+    print("[2] First status check:", status)
 
-# # Step 3: Poll for 2 minutes or until completed
-# count = 0
-# while status['status'] in ["PENDING", "RUNNING"] and count < 12:
-#     time.sleep(10)
-#     status = check_job_status(wsi_id, tool_name)
-#     if status:
-#         print(f"[{count}] Poll status check:", status)
+# Step 3: Poll for 2 minutes or until completed
+count = 3
+while status['status'] in ["PENDING", "RUNNING"] and count < 15:
+    time.sleep(3)
+    status = check_job_status(wsi_id, tool_name)
+    if status:
+        print(f"[{count}] Poll status check:", status)
         
-#     count += 1
-
-# # Step 4: Cleanup result and log files
-# if status['status'] == "COMPLETED":
-#     json_file = output_dir / f"{wsi_id}.json"
-#     slurm_log = output_dir / f"slurm_{wsi_id}.out"
-
-#     for file in [json_file, slurm_log]:
-#         if file.exists():
-#             file.unlink()
-#             print(f"[4] Deleted file: {file}")
-#         else:
-#             print(f"[4] File not found for cleanup: {file}")
+    count += 1
+    
+    
+# Step 4: Cleanup result and log files
+if status['status'] == "COMPLETED":
+    for file in output_dir.glob("*"):
+        if file.is_file():
+            file.unlink()
+            print(f"[-] Deleted file: {file}")
             
-# else:
-#     print("Because status 2 was not 'COMPLETED', output files were not deleted.")
+else:
+    print("Because status 2 was not 'COMPLETED', output files were not deleted.")
